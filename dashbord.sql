2f55148166d4f7a840aa5580351fe9e782d0628b
98Alex <moryak1998@mail.ru>
2024-08-26 09:55:54 +0300

    dashbord.sql

--- dashbord.sql
+++ dashbord.sql
@@ -1,2 +1,15 @@
-INSERT INTO leads (count) VALUES
-	 (1300);
+-- Сколько пользователей заходят на сайт
+select count(distinct visitor_id)
+from sessions
+
+-- Каналы, приводящие на сайт, по дням недели
+select count(visitor_id),
+to_char(visit_date,'Day'),
+source
+from sessions s 
+group by 2, 3
+
+-- Сколько лидов?
+select count(*) as leads_count 
+from leads l ;
+
